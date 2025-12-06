8bc25b2d13c4b99bcc08eea4852c3217aad1dc66
maksympash <maksympash@gmail.com>
2025-12-06 12:54:22 +0200

    initial commit

---
+++ app/src/main/java/com/example/privatcurrency/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.privatcurrency
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.privatcurrency.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+    }
+}
